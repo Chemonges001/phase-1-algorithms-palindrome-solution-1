228da546edc922721fb19cbfee1672198175035f
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-18 16:18:05 +0300

    commit tree 75ef5016dc8e063b47dfced741b2e00fe8b8c2d8

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+ 
+  const reversedWord = reverseString(word);
+
+  return word === reversedWord;
+
 }
 
+function reverseString(word) {
+  return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
 */
+// reverse the input string
 
+// if the reversed string is equal to the input
+//   return true
+// else
+//   return false
 /*
   Add written explanation of your solution here
+  the reverseWord variable reverses the input string and returs a word that is equal to the initial word.
+  fucntion reverseString return a reverses word. 
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
